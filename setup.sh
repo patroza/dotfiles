52d4f9858c5c5812729664241d5887b26db5e166
Patrick Roza <contact@patrickroza.com>
2025-09-30 20:29:21 +0200

    add fonts

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ brew tap FelixKratz/formulae
 brew install borders
 
 brew install stow
+
+brew install --cask font-ubuntu-mono-nerd-font
+brew install --cask font-hack-nerd-font
+brew install --cask font-sf-pro
+brew install --cask font-sketchybar-app-font
