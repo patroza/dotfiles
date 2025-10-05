93f08a76e395289987de8b10b8847fe0373a4b5e
Patrick Roza <contact@patrickroza.com>
2025-10-05 21:30:27 +0200

    add simple-bar support

---
+++ aerospace/.config/aerospace/workspace-change.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl "http://localhost:7776/aerospace/spaces/refresh?space=$AEROSPACE_FOCUSED_WORKSPACE"
+sketchybar --trigger aerospace_workspace_change FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE
