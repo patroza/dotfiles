2ca49bd496205c7e4360f131ffc4ef048f568bfd
Patrick Roza <contact@patrickroza.com>
2025-10-05 21:14:09 +0200

    add also simple-bar

--- setup.sh
+++ setup.sh
@@ -12,3 +12,24 @@ brew install --cask font-ubuntu-mono-nerd-font
 brew install --cask font-hack-nerd-font
 brew install --cask font-sf-pro
 brew install --cask font-sketchybar-app-font
+
+# skychybar alt
+brew install --cask ubersicht
+git clone https://github.com/Jean-Tinland/simple-bar $HOME/Library/Application\ Support/Übersicht/widgets/simple-bar
+git clone https://github.com/Jean-Tinland/simple-bar-server.git $HOME/pj/simple-bar-server
+cd $HOME/pj/simple-bar-server
+# Install local dependencies
+npm install
+
+npm i -g pm2
+# Run the server
+npm run start
+
+# Register the server to be run at startup
+# pm2 will ask you to run a command as sudo to register it ("sudo env PATH=...")
+pm2 startup
+
+sudo env PATH=$PATH:/Users/patrickroza/.nvm/versions/node/v22.20.0/bin /Users/patrickroza/.nvm/versions/node/v22.20.0/lib/node_modules/pm2/bin/pm2 startup launchd -u patrickroza --hp /Users/patrickroza
+
+# Save the current pm2 configuration
+pm2 save
