360cbcb80d371e89f69f73181f8f69e2e0c947ca
Patrick Roza <contact@patrickroza.com>
2025-10-24 15:39:24 +0200

    interesting toggle script

---
+++ aerospace/.config/aerospace/toggle-app.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# from https://github.com/nikitabobko/AeroSpace/issues/296
+# also has more interesting ideas for yabai.. which maybe can be adapted? https://github.com/farhantamzid/dotfiles/blob/main/yabai/.config/yabai/toggle_safari.sh
+
+APP_ID=$1
+APP_NAME=$2
+
+focus_app() {
+  current_workspace=$(aerospace list-workspaces --focused)
+  app_window_id=$(aerospace list-windows --all --format "%{window-id}%{right-padding} | %{app-name}" | grep $APP_NAME | cut -d' ' -f1 | sed '1p;d')
+  aerospace focus --window-id $app_window_id
+  aerospace move-node-to-workspace $current_workspace
+  aerospace workspace $current_workspace
+  aerospace move-mouse window-lazy-center
+}
+
+app_closed() {
+  if [ "$(aerospace list-windows --all --format '%{app-name}' | grep $APP_NAME)" == "" ]; then
+    true
+  else
+    false
+  fi
+}
+
+app_focused() {
+  if [ "$(aerospace list-windows --focused --format "%{app-bundle-id}")" == "$APP_ID" ]; then
+    true
+  else
+    false
+  fi
+}
+
+unfocus_app() {
+  aerospace move-node-to-workspace scratchpad
+}
+
+if app_closed; then
+  open -a $APP_NAME
+  sleep 0.5
+else
+  if app_focused; then
+    unfocus_app
+  else
+    focus_app
+  fi
+fi
